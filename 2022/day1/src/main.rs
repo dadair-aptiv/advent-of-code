8108c80afe5d2ce3a18cd8a19864bb43336abb32
Devon Adair <devon.adair@aptiv.com>
2022-12-01 17:24:09 -0500

    Day 1 moving this from scratch directory to new repo for the advent of code. Cleaned up the solution to take in the input and optimized the solution some.

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,70 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt")
+        .expect("Can't read the input file");
+
+    let sorted_calories = sort_calories(contents);
+
+    let top_three = sorted_calories[0] + sorted_calories[1] + sorted_calories[2];
+    let max = sorted_calories[0];
+    println!("Top three = {:?}", top_three);
+    println!("Max = {:?}", max);
+
+    assert_eq!(top_three, 199172);
+    assert_eq!(max, 66616);
+}
+
+fn sort_calories(calorie_list: String) -> Vec<usize> {
+    let mut elves = vec![];
+    let mut total_calories = 0;
+    for line in calorie_list.lines() {
+        if line.is_empty() {
+            elves.push(total_calories);
+            total_calories = 0;
+        } else {
+            let calories = line.parse::<usize>().unwrap();
+            total_calories += calories;
+        }
+    }
+
+    // Last one isn't empty so not stored
+    elves.push(total_calories);
+
+    // Sort list largest to smallest
+    elves.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    return elves;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::sort_calories;
+
+    #[test]
+    fn example_calorie_list() {
+        let calorie_list = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        let calorie_count = sort_calories(calorie_list.to_string());
+
+        assert_eq!(calorie_count[0], 24000);
+        assert_eq!(calorie_count[1], 11000);
+        assert_eq!(calorie_count[2], 10000);
+
+        let top_three = calorie_count[0] + calorie_count[1] + calorie_count[2];
+        assert_eq!(top_three, 45000);
+    }
+
+}
